6b659dca12f59fe00df2c0212b0a20476800a829
Oluyalireuben <oluyalireuben1@gmail.com>
2023-11-10 20:45:22 +0300

    first commit

---
+++ app/src/main/java/com/example/javaquiz/ResultActivity.java
@@ -0,0 +1,56 @@
+package com.example.javaquiz;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class ResultActivity extends AppCompatActivity {
+    TextView tv, tv2, tv3;
+    Button btnRestart, btnLogOut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+
+        tv = (TextView)findViewById(R.id.tvres);
+        tv2 = (TextView)findViewById(R.id.tvres2);
+        tv3 = (TextView)findViewById(R.id.tvres3);
+        btnRestart = (Button) findViewById(R.id.btnRestart);
+        btnLogOut = (Button) findViewById(R.id.btnLogOut);
+
+        StringBuffer sb = new StringBuffer();
+        sb.append("Correct answers: " + QuestionsActivity.correct + "\n");
+        StringBuffer sb2 = new StringBuffer();
+        sb2.append("Wrong Answers: " + QuestionsActivity.wrong + "\n");
+        StringBuffer sb3 = new StringBuffer();
+        sb3.append("Final Score: " + QuestionsActivity.correct + "\n");
+        tv.setText(sb);
+        tv2.setText(sb2);
+        tv3.setText(sb3);
+
+        QuestionsActivity.correct=0;
+        QuestionsActivity.wrong=0;
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(getApplicationContext(),QuestionsActivity.class);
+                startActivity(in);
+            }
+        });
+
+        btnLogOut.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent in = new Intent(getApplicationContext(),LoginActivity.class);
+                startActivity(in);
+            }
+        });
+    }
+
+}
